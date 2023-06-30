64394af3b165dd58fe87749c6b0e4e415c230cbb
Aliona Kragel <aliona.kragel@mail.ru>
2023-06-30 03:09:49 +0300

    Added some UI changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,22 @@
 import Image from "next/image";
+import styles from "./styles.module.scss"
+import { headers } from "next/dist/client/components/headers";
 
 const About = () => {
   return (
-    <div>
-      <div>
-
-      </div>
-      <div>
+    <div className={styles.about__wrapper}>
+      <div className={styles.about__info}>
         <h1>About</h1>
-        <p>Welcome to the Test Store, a collection of apparel and accessories designed for test task. </p>
+        <p className={styles.about__slogan}>Welcome to the Test Store, a collection of apparel and accessories designed for test task. </p>
       </div>
-
+      <div className={styles.about__background}>
+        <Image
+          src={`/images/about-page.jpg`}
+          alt={`about`}
+          width={350}
+          height={800}
+          className={styles.about__background_img} />
+      </div>
     </div>
 
   )
